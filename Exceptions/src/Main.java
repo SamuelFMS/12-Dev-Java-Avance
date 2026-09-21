073b6dd5722131951a6c26e073af9209b995b426
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-21 15:31:34 +0200

    p

--- Exceptions/src/Main.java
+++ Exceptions/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Date date = null;
         Date today = Date.from(Instant.now()); // new Date() est correcte mais sonarqube me met une erreur pas content
-        try() {
+        try {
             System.out.println(date.getClass().getName()); // Un Objet null n'est pas instancié et donc ne possede pas les classes meres
         }
         catch (NullPointerException e){
