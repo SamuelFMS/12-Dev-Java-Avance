0e3b41fc7f3b0ce081376d45ce14bb6259ed9013
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-21 15:15:28 +0200

    new Main for test

--- ManipUneBDD/src/Main.java
+++ ManipUneBDD/src/Main.java
@@ -4,11 +4,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         try {
             Class.forName("org.mariadb.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -19,9 +21,29 @@ public class Main {
 
 
         // Affichages de tous les articles
-        articles.forEach(article -> System.out.println(article));
+        ArticleDB.getAll().forEach(article -> System.out.println(article));
+        scanner.nextLine();
+
         // Insertion dun article
+        System.out.println("Insertion d'un nouveau Article");
         Article newArticle = new Article("Description", "Marque", 10);
-        ArticleDB.deleteArticle(13);
+        ArticleDB.updateOrCreate(newArticle);
+        ArticleDB.getAll().forEach(article -> System.out.println(article));
+        scanner.nextLine();
+
+        // Suppression
+        System.out.println("Suppression de cet article");
+        ArticleDB.deleteArticle(newArticle);
+        ArticleDB.getAll().forEach(article -> System.out.println(article));
+        scanner.nextLine();
+
+        // Edition de l'article 0
+        if(articles.get(0).getRsBrand().equals("Logic")) {
+            articles.get(0).setRsBrand("Logitoch");
+        }else {
+            articles.get(0).setRsBrand("Logic");
+        }
+        ArticleDB.updateOrCreate(articles.get(0));
+        ArticleDB.getAll().forEach(article -> System.out.println(article));
     }
 }
