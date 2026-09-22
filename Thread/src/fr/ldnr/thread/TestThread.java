221a71c78499c10ff7b6886a93c46a77fa41cea0
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-22 08:57:52 +0200

    Refait test thread

--- Thread/src/fr/ldnr/thread/TestThread.java
+++ Thread/src/fr/ldnr/thread/TestThread.java
@@ -2,38 +2,31 @@ package fr.ldnr.thread;
 
 import java.util.ArrayList;
 
-public class TestThread {
-
+public class TestThread extends Thread {
+    public TestThread(String name){
+        super(name);
+    }
+    public void run(){
+        for(int i = 0; i < 5; i++) {
+            System.out.print(this.getName());
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
+
         ArrayList<Thread> threads= new ArrayList<>();
         for(int i = 1; i <= 5; i++){
-            Thread thread = new Thread(new MonRunnable(i));
+            StringBuilder display = new StringBuilder().append(i);
+            for (int x = 0; x<i; x++){
+                display.append("-");
+            }
+            display.append(" ");
+
+            Thread thread = new Thread(new TestThread(display.toString()));
             threads.add(thread);
         }
         for (Thread thread: threads){
             thread.start();
-        }
-    }
-
-    private static class MonRunnable implements Runnable {
-
-        private final long number;
-
-        public MonRunnable(int number) {
-            this.number = number;
-        }
-
-        @Override
-        public void run() {
-            StringBuilder display = new StringBuilder().append(number);
-            for (int i = 0; i<number; i++){
-                display.append("-");
-            }
-            display.append(" ");
-            for(int i = 0; i < 5; i++) {
-                System.out.print(display.toString());
-            }
-            System.out.println();
         }
     }
 }
